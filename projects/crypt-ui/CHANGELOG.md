714ba9835ab628d5b8c4a85abfa8dcaede710e80
Brian <brianb.james@mac.com>
2024-11-20 03:32:16 -0800

    Crypt-UI

---
+++ projects/crypt-ui/CHANGELOG.md
@@ -0,0 +1,15 @@
+# Changelog
+
+## 1.1.0
+
+### Features
+
+- Add chip component
+
+### Fixes
+
+- Prevent form field label text wrapping
+
+## 1.0.0
+
+- Initial release
---
+++ projects/crypt-ui/src/components/core-input/implementation/CoreInput.svelte
@@ -0,0 +1,14 @@
+<svelte:options tag={null} />
+
+<script lang="ts">
+  export let size: "small" | "large" = "small";
+  export let color: "primary" | "secondary" | "success" | "danger" = "primary";
+</script>
+
+<div class="crypt-core-input {size} {color}">
+  <input {...$$restProps} />
+</div>
+
+<style lang="scss">
+  @use "core-input";
+</style>
