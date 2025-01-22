81d4e2b694671e809d9387ae06cdd76c51eebafa
Brian <brianb.james@mac.com>
2025-01-21 16:13:15 -0800

    Clean Up

--- projects/crypt-ui/src/components/chip/chip.stories.ts
+++
@@ -1,9 +0,0 @@
-import "./chip.scss";
-import chipColors from "./chip-colors.html";
-
-export default {
-  title: "Components/Chip",
-  parameters: { layout: "centered" },
-};
-
-export const colors = () => chipColors;
